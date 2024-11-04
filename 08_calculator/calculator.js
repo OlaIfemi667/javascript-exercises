83240b27878803393815763f64611317e72e7a02
Ola <bancolejeandedieu@gmail.com>
2024-11-04 11:32:08 +0100

    fix: factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.length === 0 ? 0 : numbers.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0) ;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  return numbers.length === 0 ? 0 : numbers.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1)
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if(a == 0)
+    return 1;
+  return a * factorial(a-1);
 	
 };
 
